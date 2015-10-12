8bff5acb58e60e031c5d11ad9ec6a98b7e994e97
mcgrandlej <mcgrandlej@gmail.com>
2015-10-12 16:17:31 -0700

    Have the navbar load certain scripts

--- www/js/navBar.js
+++ www/js/navBar.js
@@ -1,10 +1,14 @@
+//We need to load the page scripts here, as loading them in the page produces a deprecated warning
+var profileJSURL = "js/profile.js";
+
 $(document).ready(function() {
-  console.log('Doc Ready');
+  console.log('Navbar ready to go');
   $("#homeButton").click(function(){
     $("#mainContent").load("home.html");
   });
   $("#profileButton").click(function(){
     $("#mainContent").load("profile.html");
+    $.getScript(profileJSURL);
   });
   $("#createButton").click(function(){
     $("#mainContent").load("createEvent.html");
