4a9e63c4185cb50887a3d1e47b60193ac77ddf12
mcgrandlej <mcgrandlej@gmail.com>
2015-10-15 16:12:04 -0700

    Since the CSP was removed, we can now put inline JS in the pages

--- client/www/js/navBar.js
+++ client/www/js/navBar.js
@@ -1,6 +1,3 @@
-//We need to load the page scripts here, as loading them in the page produces a deprecated warning
-var profileJSURL = "js/profile.js";
-
 $(document).ready(function() {
   console.log('Navbar ready to go');
   $("#homeButton").click(function(){
@@ -8,7 +5,6 @@ $(document).ready(function() {
   });
   $("#profileButton").click(function(){
     $("#mainContent").load("profile.html");
-    $.getScript(profileJSURL);
   });
   $("#createButton").click(function(){
     $("#mainContent").load("createEvent.html");
