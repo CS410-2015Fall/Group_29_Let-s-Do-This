11050d3c6790ff98177cc4697014faef6836fa1e
Graham Bing <grrraham@gmail.com>
2015-10-18 00:14:52 -0700

    adjusted styling, added hook for notification or event links

--- client/www/js/home.js
+++ client/www/js/home.js
@@ -1,16 +1,16 @@
 var tempFakeNotificationData = [
-{head:"Notification1",body:"Tom's going to your christmas party!"},
-{head:"Notification2",body:"Dick invited you to trivia night at the cambie!"},
-{head:"Notification3",body:"Harry's Birthday Party is today!"},
-{head:"Notification4",body:"Sally commented on your christmas party"}];
+{head:"Notification1",body:"Tom's going to your christmas party!",eventId:"1"},
+{head:"Notification2",body:"Dick invited you to trivia night at the cambie!",eventId:"2"},
+{head:"Notification3",body:"Harry's Birthday Party is today!",eventId:"3"},
+{head:"Notification4",body:"Sally commented on your christmas party",eventId:"4"}];
 
 var tempFakeEventData = [
-{head:"Event1",body:"this is an event on a day"},
-{head:"Event2",body:"another event on another day"},
-{head:"Event3",body:"yup, you guessed it"},
-{head:"These boxes are cool",body:"because they're generated dynamically from an array of strings"}];
+{head:"Event1",body:"this is an event on a day",eventId:""},
+{head:"Event2",body:"another event on another day",eventId:""},
+{head:"Event3",body:"yup, you guessed it",eventId:""},
+{head:"These boxes are cool",body:"because they're generated dynamically from an array of strings",eventId:""}];
 
-var friends = ["the styling","is clearly","not loading","correctly","click the x","in \"Filter Items...\"","Tom","Dick","Harry","Sally","Wolfgang","Emil","Mathias","Magnus","Jonas","William","Oliver","Noah","Adrian","Tobias","Elias","Daniel","Henrik","Sebastian","Lucas","Martin","Andreas","Benjamin","Leon","Sander","Alexander","Liam","Isak","Jakob","Kristian","Aksel","Julian","Fredrik","Sondre","Johannes","Erik","Marius","Jonathan","Filip"];
+var tempFakeFriends = ["the styling","is clearly","not loading","correctly","click the x","in \"Filter Items...\"","Tom","Dick","Harry","Sally","Wolfgang","Emil","Mathias","Magnus","Jonas","William","Oliver","Noah","Adrian","Tobias","Elias","Daniel","Henrik","Sebastian","Lucas","Martin","Andreas","Benjamin","Leon","Sander","Alexander","Liam","Isak","Jakob","Kristian","Aksel","Julian","Fredrik","Sondre","Johannes","Erik","Marius","Jonathan","Filip"];
 
 
 $(document).ready(function() {
@@ -35,13 +35,21 @@ $(document).ready(function() {
     });
     $("#friendsButton").click(function(){
     });
+
+    $("#box").click(function(){
+
+    });
+
+    createContentBoxLinks();
 });
 
 function createContentBoxes(boxes) {
     $("#mainContent").html("");
     $.each( boxes, function( index, value ){
         $("#mainContent").append(
-            '<div id="box"><p><strong>'
+            '<div id="box" eventId="'
+            + value.eventId
+            +'"><p><strong>'
             + value.head
             + '</strong><br>'
             + value.body
@@ -49,9 +57,18 @@ function createContentBoxes(boxes) {
     });
 }
 
+function createContentBoxLinks() {
+    $(document).on("click", '#mainContent div', function(e) {
+        if ($(this).attr("id") == "box") {
+            // generate link from eventId
+            alert($(this).attr("eventId"));
+        }
+    });
+}
+
 function loadFriends() {
     $("#friends").html();
-    $.each( friends, function( index, value ){
+    $.each( tempFakeFriends, function( index, value ){
         $("#friendList").append(
             '<li><a href="">'
             + value
