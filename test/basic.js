119bbac1be9d5e2a7f84f96fb848ddc02d10895a
shaybeau731 <shaybeau731@gmail.com>
2015-11-18 02:44:19 -0800

    added jquery dependencies to package.json

--- test/basic.js
+++ test/basic.js
@@ -1,9 +1,17 @@
 // For describe, it and before/after see:
 // http://samwize.com/2014/02/08/a-guide-to-mochas-describe-it-and-setup-hooks/
-var assert = require('chai').assert;
+//var assert = require('chai').assert;
 
 //The most basic of tests
-describe('basic tests', function(){
-  assert.equal(1+1, 2);
-  assert.equal(true, 1<2);
-})
+//describe('basic tests', function(){
+//  assert.equal(1+1, 2);
+//  assert.equal(true, 1<2);
+//})
+
+describe('testing date formatting', function () {
+  var formattedDate = formatTime("2015-11-17", "20:05");
+  
+  it('Should have a T between date and time', function(){
+    expect(formattedDate).toBe("2015-11-17T20:05");
+  });
+});
