c18d1fcf5a5ba0a8a228c905e982e78e62795610
shaybeau731 <shaybeau731@gmail.com>
2015-11-18 02:59:06 -0800

    I just want to see what happens when there aren't errors

--- client/www/js/user/settings.js
+++ client/www/js/user/settings.js
@@ -2,5 +2,5 @@ var LetsDoThis = LetsDoThis || {};
 LetsDoThis.Settings = LetsDoThis.Settings || {};
 
 var LDTSERVER = "http://159.203.12.88/api";
-LetsDoThis.Settings.logInUrl = LTDSERVER+"/users/"
-LetsDoThis.Settings.createEventUrl = LTDSERVER+"/events/"
+//LetsDoThis.Settings.logInUrl = LTDSERVER+"/users/"
+//LetsDoThis.Settings.createEventUrl = LTDSERVER+"/events/"
