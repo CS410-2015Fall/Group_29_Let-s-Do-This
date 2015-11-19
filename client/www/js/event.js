99f1323527b867118b5be2ace13382a1ecb45e2f
mcgrandlej <mcgrandlej@gmail.com>
2015-11-18 21:50:43 -0800

    Map support to have a marker placed where the event is

--- client/www/js/event.js
+++ client/www/js/event.js
@@ -1,37 +1,39 @@
 $.getScript("js/global.js", function() {
-    $(document).ready(function() {
-        var eventData = JSON.parse( localStorage.getItem("eventObj") );
-        console.log(eventData);
-        loadEventData(eventData);
+  $(document).ready(function() {
+    var eventData = JSON.parse( localStorage.getItem("eventObj") );
+    console.log(eventData);
+    loadEventData(eventData);
 
-        $("#inviteButton").click(function(){
-            var invited = getGuestCheckboxValues();
-            // TODO set changed users to invited/uninvited
-            // do not call function if no users were invited
-            if (invited.length != 0) {
-                inviteFriends(eventData,invited);
-            };
-            $("#friendsPopup").popup("close");
-        });
-
-        $("#homeButton").click(function(){
-            window.location="home.html";
-        });
-
-        $("#viewMap").click(function(){
-            window.location="map.html";
-        });
-
-        $("#rsvpButton").click(function(){
-            var userInfo = LetsDoThis.Session.getInstance().getUserInfo();;
-            handleRsvp(eventData,userInfo);
-        });
-
-        $("#commentForm").submit(function(event) {
-            event.preventDefault(); // do not redirect
-            postComment(eventData);
-        });
+    $("#inviteButton").click(function(){
+      var invited = getGuestCheckboxValues();
+      // TODO set changed users to invited/uninvited
+      // do not call function if no users were invited
+      if (invited.length != 0) {
+        inviteFriends(eventData,invited);
+      };
+      $("#friendsPopup").popup("close");
     });
+
+    $("#homeButton").click(function(){
+      window.location="home.html";
+    });
+
+    $("#viewMap").click(function(){
+      //Save the location, and name, into the session storage so the map script can pull it up
+
+      window.location="map.html";
+    });
+
+    $("#rsvpButton").click(function(){
+      var userInfo = LetsDoThis.Session.getInstance().getUserInfo();;
+      handleRsvp(eventData,userInfo);
+    });
+
+    $("#commentForm").submit(function(event) {
+      event.preventDefault(); // do not redirect
+      postComment(eventData);
+    });
+  });
 });
 
 function loadEventData(e) {
@@ -49,157 +51,157 @@ function loadEventData(e) {
   var userId = LetsDoThis.Session.getInstance().getUserId();
   if ($.inArray(userId,e.accepts) == 0) {
     $("#rsvpButton").attr('disabled', 'true');
-}
+  }
 
-var comments = formatComments(e.comments);
-createContentBoxes(comments,$("#comments"));
+  var comments = formatComments(e.comments);
+  createContentBoxes(comments,$("#comments"));
 }
 
 function loadGuests(event) {
 
-    var friendIds = LetsDoThis.Session.getInstance().getUserFriends();
+  var friendIds = LetsDoThis.Session.getInstance().getUserFriends();
 
-    // reduce friendIds so that it contains only those ids which are not already invited, attending or declining the event
-    function reduceList(l1,l2) {
-        $.each(l2, function(index, val) {
-            l1 = jQuery.grep(l1, function(value) {
-                return value != val;
-            });
-        });
-        return l1;
-    }
+  // reduce friendIds so that it contains only those ids which are not already invited, attending or declining the event
+  function reduceList(l1,l2) {
+    $.each(l2, function(index, val) {
+      l1 = jQuery.grep(l1, function(value) {
+        return value != val;
+      });
+    });
+    return l1;
+  }
 
-    friendIds = reduceList(friendIds,event.invites);
-    friendIds = reduceList(friendIds,event.accepts);
-    friendIds = reduceList(friendIds,event.declines);
+  friendIds = reduceList(friendIds,event.invites);
+  friendIds = reduceList(friendIds,event.accepts);
+  friendIds = reduceList(friendIds,event.declines);
 
-    updateGuestListUi(event.accepts,event.invites,friendIds,event.declines);
+  updateGuestListUi(event.accepts,event.invites,friendIds,event.declines);
 }
 
 function updateGuestListUi(accepts,invites,friends,declines) {
 
-    //var accepts = [];
-    //var invites = [];
-    //var friends = [];
-    //var declines = [];
+  //var accepts = [];
+  //var invites = [];
+  //var friends = [];
+  //var declines = [];
 
-    // get user information corresponding to the userIds
-    //function mapUsers(userIds, users) {
-    //    jQuery.map(userIds, function(val,key){
-    //        getUser(val, function(resp) {
-    //            var u = {
-    //                username:resp.username,
-    //                user_id:val
-    //            }
-    //            users.push(u);
-    //        });
-    //    });
-    //}
+  // get user information corresponding to the userIds
+  //function mapUsers(userIds, users) {
+  //    jQuery.map(userIds, function(val,key){
+  //        getUser(val, function(resp) {
+  //            var u = {
+  //                username:resp.username,
+  //                user_id:val
+  //            }
+  //            users.push(u);
+  //        });
+  //    });
+  //}
 
-    //mapUsers(acceptIds,accepts);
-    //mapUsers(inviteIds,invites);
-    //mapUsers(friendIds,friends);
-    //mapUsers(declineIds,declines);
-    //create html for list of users associated with event
-    function write(list, attrs, status) {
-        var str = "";
-        $.each(list, function(i, user) {
-            str += '<input type="checkbox" name="accept" id="' + user.id + '" value="' + user.id + '" ' + attrs + '><label for="' + user.id + '">' + user.username + ' ' + status + '</label>';
-        });
-        return str;
-    }
+  //mapUsers(acceptIds,accepts);
+  //mapUsers(inviteIds,invites);
+  //mapUsers(friendIds,friends);
+  //mapUsers(declineIds,declines);
+  //create html for list of users associated with event
+  function write(list, attrs, status) {
+    var str = "";
+    $.each(list, function(i, user) {
+      str += '<input type="checkbox" name="accept" id="' + user.id + '" value="' + user.id + '" ' + attrs + '><label for="' + user.id + '">' + user.username + ' ' + status + '</label>';
+    });
+    return str;
+  }
 
-    var s = "";
-    s += write(accepts, ' checked="true" disabled="true"', '(attending)');
-    s += write(invites, ' checked="true"', '(invited)');
-    s += write(friends, '', '');
-    s += write(declines, ' disabled="true"', '(not attending)');
-    $("fieldset#friendsPopup").html(s);
-    $("#friendsPopup").trigger('create');
+  var s = "";
+  s += write(accepts, ' checked="true" disabled="true"', '(attending)');
+  s += write(invites, ' checked="true"', '(invited)');
+  s += write(friends, '', '');
+  s += write(declines, ' disabled="true"', '(not attending)');
+  $("fieldset#friendsPopup").html(s);
+  $("#friendsPopup").trigger('create');
 }
 
 function getGuestCheckboxValues() {
-    var ancestor = document.getElementById('friendsPopup'),
-    descendents = ancestor.getElementsByTagName('input');
-    var i, e;
-    var invited = [];
-    for (i = 0; i < descendents.length; ++i) {
-        e = descendents[i];
-        if (e.checked) {
-            invited.push(e.id)
-        }
+  var ancestor = document.getElementById('friendsPopup'),
+  descendents = ancestor.getElementsByTagName('input');
+  var i, e;
+  var invited = [];
+  for (i = 0; i < descendents.length; ++i) {
+    e = descendents[i];
+    if (e.checked) {
+      invited.push(e.id)
     }
-    return invited;
+  }
+  return invited;
 }
 
 function postComment(event, comment) {
-    var date = currentDate();
-    var author = LetsDoThis.Session.getInstance().getUserInfo();
-    var newComment = {
-        author: author,
-        post_date: date,
-        content: $('textarea#commentTextArea').val()
-    };
+  var date = currentDate();
+  var author = LetsDoThis.Session.getInstance().getUserInfo();
+  var newComment = {
+    author: author,
+    post_date: date,
+    content: $('textarea#commentTextArea').val()
+  };
 
-    // update UI
-    getAllComments(event.id, function(resp) {
-        resp.push(newComment);
-        var uiFormattedComments = formatComments(resp);
-        createContentBoxes(uiFormattedComments,$("#comments"));
-        $('textarea#commentTextArea').val("");
-    });
-    // var comments = event.comments; // if we get comments from the server here we will update with any other comments posted since we last reloaded from it
-    // comments.push(newComment);
+  // update UI
+  getAllComments(event.id, function(resp) {
+    resp.push(newComment);
+    var uiFormattedComments = formatComments(resp);
+    createContentBoxes(uiFormattedComments,$("#comments"));
+    $('textarea#commentTextArea').val("");
+  });
+  // var comments = event.comments; // if we get comments from the server here we will update with any other comments posted since we last reloaded from it
+  // comments.push(newComment);
 
-    // send comment to server
-    addComment(event.id,
-        LetsDoThis.Session.getInstance().getUserId(),
-        date,
-        newComment.content);
-}
+  // send comment to server
+  addComment(event.id,
+    LetsDoThis.Session.getInstance().getUserId(),
+    date,
+    newComment.content);
+  }
 
-function formatComments(comments) {
+  function formatComments(comments) {
     var formattedComments = [];
     $.each( comments, function( index, comment ){
-        var h = comment.author.username + " commented at " + convertDate(comment.post_date);
+      var h = comment.author.username + " commented at " + convertDate(comment.post_date);
 
-        var c = {head: h,
-            body:comment.content,
-            boxId:""};
-            formattedComments.push(c);
-        });
-    return formattedComments;
-}
+      var c = {head: h,
+        body:comment.content,
+        boxId:""};
+        formattedComments.push(c);
+      });
+      return formattedComments;
+    }
 
-function handleRsvp(e,userInfo) {
-    e.invites = jQuery.grep(e.invites, function(value) {
+    function handleRsvp(e,userInfo) {
+      e.invites = jQuery.grep(e.invites, function(value) {
         return value != userInfo;
-    });
-    e.declines = jQuery.grep(e.declines, function(value) {
+      });
+      e.declines = jQuery.grep(e.declines, function(value) {
         return value != userInfo;
-    });
+      });
 
-    rsvpToEvent(e.id,'accepts', function(){
+      rsvpToEvent(e.id,'accepts', function(){
         // do something
-    });
+      });
 
-    e.accepts.push(userInfo);
-    updateGuestListUi(e.accepts,e.invites,[],e.declines);
+      e.accepts.push(userInfo);
+      updateGuestListUi(e.accepts,e.invites,[],e.declines);
 
-    $("#rsvpButton").attr('disabled', 'true');
-    $("#rsvpPopup").popup( "open" )
+      $("#rsvpButton").attr('disabled', 'true');
+      $("#rsvpPopup").popup( "open" )
 
-}
+    }
 
-function inviteFriends(e,invitedUsers) {
-    inviteToEvent(e.id, invitedUsers, function(){
+    function inviteFriends(e,invitedUsers) {
+      inviteToEvent(e.id, invitedUsers, function(){
         // on success, update event information
         // there is probably a simpler way to do this
         getEvent(e.id,function(updatedEvent){
-            localStorage.setItem("eventObj", JSON.stringify(updatedEvent));
-            eventData = JSON.parse( localStorage.getItem("eventObj") );
-            loadGuests(eventData);
+          localStorage.setItem("eventObj", JSON.stringify(updatedEvent));
+          eventData = JSON.parse( localStorage.getItem("eventObj") );
+          loadGuests(eventData);
         });
 
-    });
-}
+      });
+    }
--- client/www/js/serverInteractions/gmapsInteraction.js
+++ client/www/js/serverInteractions/gmapsInteraction.js
@@ -1,12 +1,10 @@
 var map; //The map object
-
-$(document).ready(function() {
-
-});
-
+var eventTitle;
+var eventLocation;
 document.addEventListener("deviceready", function(){
+  //Nothing can occur with this plugin until this function completes
   console.log("Google maps received deviceready");
-  //
+
   // // -------Straight stole this from https://github.com/mapsplugin/cordova-plugin-googlemaps/wiki/Map---------
   // Initialize the map plugin
   map = plugin.google.maps.Map.getMap();
@@ -15,6 +13,58 @@ document.addEventListener("deviceready", function(){
 }, false);
 
 function onMapInit(map) {
+  //The google maps utils are finally loaded
+
+  //Display the map in the appropriate container
   var mapDiv = document.getElementById('mainContent');
-	map.setDiv(mapDiv);
+  map.setDiv(mapDiv);
+
+  console.log("Searching for a latlong at: " + eventLocation);
+  //Convert the event location into a lat long
+  getLatLong(eventLocation, function(latLong){
+    //getLatLong is going to run async, so we must use this callback
+    console.log("Result is... " + latLong);
+    //And finally put the event marker on the map
+    dropMarker(eventTitle, latLong);
+  });
+}
+
+//Converts the location (name or address or something googlable), into a latLong
+//Returns the resulting lat long
+function getLatLong(location, callback){
+  //The request data
+  var request = {
+    'address': location //Ideally location would be an address, but anything will do
+  };
+  console.log('request:');
+  console.log(request);
+
+  plugin.google.maps.Geocoder.geocode(request, function(results) {
+    if (results.length) {//Make sure we found something
+      var result = results[0];
+      var position = result.position;
+
+      callback(position); //run the callback with the lat long
+    } else { //There we no results found for this location
+      alert("There was a problem locating this event!");
+    }
+  });
+}
+
+//Places a marker on the map at the latLong with the given name
+function dropMarker(title, latLong){
+  map.addMarker({
+    'title': title,
+    'position': latLong
+  }); //addMarker can also take a callback function
+  map.animateCamera({
+  'target': latLong,
+  'zoom': 15
+});
+}
+
+function initEventData(title, loc){
+  console.log("setting eventLocation as: " + loc);
+  eventTitle = title;
+  eventLocation = loc;
 }
