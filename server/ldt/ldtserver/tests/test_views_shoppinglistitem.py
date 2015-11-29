b11a19aeeb9b9b53a680ff09866682375cb18889
tk baylis <tammy.baylis@gmail.com>
2015-11-28 23:22:44 -0800

    renamed test files

--- server/ldt/ldtserver/tests/test_views_shoppinglistitem.py
+++ server/ldt/ldtserver/tests/test_views_shoppinglistitem.py
@@ -13,18 +13,16 @@ from rest_framework import status
 
 # Test db Constants
 U1 = "JohnnyTest"
-U2 = "BobbyTest"
 PWD = "test"
 EMAIL = "test@test.com"
 
 
-class ShoppingListTest(TestCase):
+class ShoppingListItemTest(TestCase):
 
     def setUp(self):
         User.objects._create_user(username=U1, password=PWD, email=EMAIL, is_staff=False, is_superuser=False)
-        User.objects._create_user(username=U2, password=PWD, email=EMAIL, is_staff=False, is_superuser=False)
 
-    def test_shoppinglist_put(self):
+    def test_shoppinglistitem_list(self):
         # Set up
         u1 = User.objects.get_by_natural_key(U1)
         uid1 = u1.id
@@ -32,147 +30,165 @@ class ShoppingListTest(TestCase):
             "id": uid1,
             "username": u1.username
         }
-        u2 = User.objects.get_by_natural_key(U2)
-        uid2 = u2.id
-        uobj2 = {
-            "id": uid2,
-            "username": u2.username
+        url = reverse('event_list')
+        data = {"display_name": "test event"}
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        eid1 = response.data["id"]
+        # GET all shoppinglistitem for event (before any created)
+        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
+        response = self.client.get(url)
+        self.assertEqual(response.data, [])
+        # POST new shoppinglistitem to event
+        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
+        data = [
+            {
+                "display_name": "hot dogs",
+                "quantity": 9001,
+                "cost": 12345678.90,
+                "supplier": uid1,
+                "ready": False
+            }
+        ]
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        self.assertIsInstance(response.data, list)
+        sli1 = response.data[0]
+        self.assertEqual(sli1["supplier"], uobj1)
+        self.assertEqual(sli1["display_name"], "hot dogs")
+        self.assertEqual(sli1["quantity"], "9001.00")
+        self.assertEqual(sli1["cost"], "12345678.90")
+        self.assertEqual(sli1["ready"], False)
+        # GET all shoppinglistitem for event (after some created)
+        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
+        response = self.client.get(url)
+        self.assertTrue(sli1 in response.data)
+        self.assertTrue(len(response.data), 1)
+        # GET event that does not exist
+        url = reverse('shoppinglistitem_list', kwargs={"pk": 9999})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        # POST without display_name (invalid)
+        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
+        data = [
+            {
+                "quantity": 9001,
+                "cost": 12345678.90,
+                "supplier": uid1,
+                "ready": False
+            }
+        ]
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # POST with invalid format in quantity, cost, ready
+        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
+        data = [
+            {
+                "display_name": "hot dogs",
+                "quantity": "abc",
+            }
+        ]
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
+        data = [
+            {
+                "display_name": "hot dogs",
+                "cost": "abc",
+            }
+        ]
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
+        data = [
+            {
+                "display_name": "hot dogs",
+                "ready": "abc"
+            }
+        ]
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # POST with nonexistent user as supplier
+        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
+        data = [
+            {
+                "display_name": "hot dogs",
+                "supplier": 9999,
+            }
+        ]
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_shoppinglistitem_detail(self):
+        # Set up
+        u1 = User.objects.get_by_natural_key(U1)
+        uid1 = u1.id
+        uobj1 = {
+            "id": uid1,
+            "username": u1.username
         }
         url = reverse('event_list')
         data = {"display_name": "test event"}
         response = self.client.post(url, json.dumps(data), content_type='application/json')
         eid1 = response.data["id"]
         url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
-        data = [{"display_name": "hot dogs"}]
+        data = [
+            {
+                "display_name": "hot dogs",
+                "quantity": 9001,
+                "cost": 12345678.90,
+                "ready": False
+            }
+        ]
         response = self.client.post(url, json.dumps(data), content_type='application/json')
         sli1 = response.data[0]
         sliid1 = sli1["id"]
-        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
-        data = [{"display_name": "cold buns"}]
-        response = self.client.post(url, json.dumps(data), content_type='application/json')
-        sli2 = response.data[0]
-        sliid2 = sli2["id"]
+        # GET shoppinglistitem
+        url = reverse('shoppinglistitem_detail', kwargs={"pk": eid1, "item_id": sliid1})
+        response = self.client.get(url)
+        self.assertEqual(response.data, sli1)
+        self.assertTrue(len(response.data), 1)
         # PUT shoppinglistitem with valid fields
-        url = reverse('shoppinglist_put', kwargs={"pk": eid1})
-        sli1 = {
-            "id": sliid1,
-            "display_name": "big hot dogs",
-            "quantity": "9001.00",
-            "cost": "12345678.90",
-            "supplier": uid1,
-            "ready": False
-        }
-        sli2 = {
-            "id": sliid2,
-            "display_name": "little cold buns",
-            "quantity": "1.00",
-            "cost": "9.99",
-            "ready": True
-        }
-        data = [sli1, sli2]
-        response = self.client.put(url, json.dumps(data), content_type='application/json')
-        sli1["supplier"] = uobj1
-        sli2["supplier"] = None
-        self.assertTrue(sli1 in response.data)
-        self.assertTrue(sli2 in response.data)
-        # PUT event that does not exist
-        url = reverse('shoppinglist_put', kwargs={"pk": 9999})
-        data = [sli1]
-        response = self.client.put(url, json.dumps(data), content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        # PUT at least one shoppinglistitem that does not exist
-        url = reverse('shoppinglist_put', kwargs={"pk": eid1})
-        data = [{
-            "id": 9999,
-            "display_name": "invisible cookies"
-        }]
-        response = self.client.put(url, json.dumps(data), content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        # PUT not as list of items
-        url = reverse('shoppinglist_put', kwargs={"pk": eid1})
+        url = reverse('shoppinglistitem_detail', kwargs={"pk": eid1, "item_id": sliid1})
         data = {
-            "id": sliid1,
-            "display_name": "singular cookie"
+                "display_name": "even more hot dogs",
+                "quantity": 9002,
+                "cost": 9.99,
+                "supplier": uid1,
+                "ready": True
         }
         response = self.client.put(url, json.dumps(data), content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["display_name"], "even more hot dogs")
+        self.assertEqual(response.data["quantity"], "9002.00")
+        self.assertEqual(response.data["cost"], "9.99")
+        self.assertEqual(response.data["supplier"], uobj1)
+        self.assertEqual(response.data["ready"], True)
         # PUT with invalid format in quantity, cost, ready
-        url = reverse('shoppinglist_put', kwargs={"pk": eid1})
-        data = [{
-            "id": sliid1,
-            "quantity": "abc"
-        }]
+        url = reverse('shoppinglistitem_detail', kwargs={"pk": eid1, "item_id": sliid1})
+        data = {"quantity": "abc"}
         response = self.client.put(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        url = reverse('shoppinglist_put', kwargs={"pk": eid1})
-        data = [{
-            "id": sliid1,
-            "cost": "abc"
-        }]
+        url = reverse('shoppinglistitem_detail', kwargs={"pk": eid1, "item_id": sliid1})
+        data = {"cost": "abc"}
         response = self.client.put(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        url = reverse('shoppinglist_put', kwargs={"pk": eid1})
-        data = [{
-            "id": sliid1,
-            "ready": "abc"
-        }]
+        url = reverse('shoppinglistitem_detail', kwargs={"pk": eid1, "item_id": sliid1})
+        data = {"ready": "abc"}
         response = self.client.put(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         # PUT with nonexistent user as supplier
-        url = reverse('shoppinglist_put', kwargs={"pk": eid1})
-        data = [{
-            "id": sliid1,
-            "supplier": 9999
-        }]
+        url = reverse('shoppinglistitem_detail', kwargs={"pk": eid1, "item_id": sliid1})
+        data = {"supplier": 9999}
         response = self.client.put(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_shopplinglist_delete(self):
-        # Set up
-        u1 = User.objects.get_by_natural_key(U1)
-        uid1 = u1.id
-        uobj1 = {
-            "id": uid1,
-            "username": u1.username
-        }
-        u2 = User.objects.get_by_natural_key(U2)
-        uid2 = u2.id
-        uobj2 = {
-            "id": uid2,
-            "username": u2.username
-        }
-        url = reverse('event_list')
-        data = {"display_name": "test event"}
-        response = self.client.post(url, json.dumps(data), content_type='application/json')
-        eid1 = response.data["id"]
-        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
-        data = [{"display_name": "hot dogs"}]
-        response = self.client.post(url, json.dumps(data), content_type='application/json')
-        sli1 = response.data[0]
-        sliid1 = sli1["id"]
-        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
-        data = [{"display_name": "cold buns"}]
-        response = self.client.post(url, json.dumps(data), content_type='application/json')
-        sli2 = response.data[0]
-        sliid2 = sli2["id"]
-        # POST to delete some items
-        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
-        response = self.client.get(url)
-        self.assertEqual(len(response.data), 2)
-        url = reverse('shoppinglist_delete', kwargs={"pk": eid1})
-        data = [sliid1]
-        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        # DELETE shoppinglistitem
+        url = reverse('shoppinglistitem_detail', kwargs={"pk": eid1, "item_id": sliid1})
+        response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        url = reverse('shoppinglistitem_list', kwargs={"pk": eid1})
+        # GET event that does not exist
+        url = reverse('shoppinglistitem_detail', kwargs={"pk": 9999, "item_id": sliid1})
         response = self.client.get(url)
-        self.assertEqual(len(response.data), 1)
-        # POST event that does not exist
-        url = reverse('shoppinglist_delete', kwargs={"pk": 9999})
-        data = [sliid2]
-        response = self.client.post(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        # POST at least one shoppinglistitem that does not exist
-        url = reverse('shoppinglist_delete', kwargs={"pk": eid1})
-        data = [9999]
-        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        # GET event shoppinglistitem that does not exist
+        url = reverse('shoppinglistitem_detail', kwargs={"pk": eid1, "item_id": 9999})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
